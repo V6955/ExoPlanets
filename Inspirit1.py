04dd9e44e56b4e9f4d86c41e0e8364bb74ccc338
V6955 <75700479+V6955@users.noreply.github.com>
2023-07-06 21:39:37 -0400

    Add files via upload

---
+++ Inspirit1.py
@@ -0,0 +1,32 @@
+#@title Run this code to get started
+%tensorflow_version 2.x
+!wget 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20Planet%20Hunters/exoTrain.csv'
+!wget 'https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%206%20-%2010%20(Projects)/Project%20-%20Planet%20Hunters/exoTest.csv'
+
+from urllib.request import urlretrieve
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.cluster import KMeans
+from sklearn import  metrics
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from scipy.signal import savgol_filter
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, precision_score,recall_score,f1_score
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, normalize
+
+import warnings
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('exoTrain.csv')
+df_train.LABEL = df_train.LABEL -1
+df_test = pd.read_csv('exoTest.csv')
+df_test.LABEL = df_test.LABEL - 1
